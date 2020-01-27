6c0760a9f6b152dcebb7255d984171b6307ebf4b
Deyveson Willian <deyvesonn@outlook.com>
2020-01-27 13:58:06 -0400

    subindo back-end

---
+++ back-end/src/main/java/com/br/Virtual/Store/controller/StoreController.java
@@ -0,0 +1,43 @@
+package com.br.Virtual.Store.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/store")
+public class StoreController {
+
+//
+//    @Autowired
+//    private StoreService service;
+
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String registerProduct (@RequestParam(value = "Produto", defaultValue = "", required = false) String nome){
+
+        return "Cadastrar";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public String searchProduct (@RequestParam(value = "Produto", defaultValue = "", required = false) String nome){
+
+        return "Pesquisar";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+    public String deleteProduct (@RequestParam(value = "Produto", defaultValue = "", required = false) String nome){
+
+        return "Excluir";
+    }
+
+    @RequestMapping(value = "/alter", method = RequestMethod.PUT)
+    public String alterProduct (@RequestParam(value = "Produto", defaultValue = "", required = false) String nome){
+
+        return "Alterar";
+    }
+
+}
---
+++ back-end/src/test/java/com/br/Virtual/Store/VirtualStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.Virtual.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
